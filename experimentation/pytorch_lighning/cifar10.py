2a6b3c703219e8b3ff9995f3c66800ed2a77367e
pytholic <rajahaseeb147@gmail.com>
2023-02-20 11:46:29 +0900

    pytorch-lightning example with cifar10

---
+++ experimentation/pytorch_lighning/cifar10.py
@@ -0,0 +1,95 @@
+import os
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets, transforms
+from torchvision.datasets import CIFAR10
+
+import pytorch_lightning as pl
+
+
+class LightningCIFAR10Classifier(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+
+        x = torch.randn(3, 32, 32).view(-1, 3, 32, 32)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def convs(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = self.dropout1(x)
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = self.dropout2(x)
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+    def cross_entropy_loss(self, logits, labels):
+        return F.nll_loss(logits, labels)
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        logits = self.forward(x)
+        loss = self.cross_entropy_loss(logits, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        logits = self.forward(x)
+        loss = self.cross_entropy_loss(logits, y)
+        self.log("val_loss", loss)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+class CIFAR10DataModule(pl.LightningDataModule):
+    def setup(self, stage):
+        # transforms for images
+        transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+        )
+
+        # prepare transforms standard to MNIST
+        self.cifar10_train = CIFAR10(
+            root="../data", train=True, download=False, transform=transform
+        )
+        self.cifar10_test = CIFAR10(
+            root="../data", train=False, download=False, transform=transform
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.cifar10_train, batch_size=64)
+
+    def val_dataloader(self):
+        return DataLoader(self.cifar10_test, batch_size=64)
+
+
+data_module = CIFAR10DataModule()
+
+# train
+model = LightningCIFAR10Classifier()
+trainer = pl.Trainer()
+
+trainer.fit(model, data_module)
